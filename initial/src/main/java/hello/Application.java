122823ef6e905e5c573dc893ab86da6519dec572
Ubuntu <jenkins@jenkins.tvhkrzpslhpetdkr1pzakde2aa.ix.internal.cloudapp.net>
2019-05-28 09:23:16 +0000

    add endpoint

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RSpringBootApplication
+@SpringBootApplication
 @RestController
 public class Application {
 	public static void main(String[] args) {
